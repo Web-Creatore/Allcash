4bf20bf9999b6753edaed706ef4d767873fe4d08
rupam frontend <rupam@webcreatoredigitalsolutions.com>
2024-01-23 12:49:08 +0530

    new changes

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,7 @@
 var clickbtn = document.getElementById("open-dial-btn");
+var clickbtntwo= document.getElementById("open-dial-btn-two");
+var clickbtnthree= document.getElementById("open-dial-btn-three");
+var clickbtnfour= document.getElementById("open-dial-btn-four");
 var tableSec = document.getElementById("table-sec");
 var formSec = document.getElementById("form-sec");
 var nav = document.getElementById("nav");
@@ -12,6 +15,57 @@ var mobNavClose = document.getElementById("mob-nav-close");
 
 if(clickbtn){clickbtn.addEventListener("click", myfunction);}
 function myfunction() {
+
+
+    tableSec.style.display = "none";
+    formSec.style.display = "none";
+    nav.style.display = "none";
+    footer.style.display = "none";
+
+
+    // var body=document.getElementsByTagName("body")[0];
+    dialogueBox.style.display = "flex"
+
+
+    // dialogueBox.style.position="absolute"
+
+}
+if(clickbtntwo){clickbtntwo.addEventListener("click", myfunctiontwo);}
+function myfunctiontwo() {
+
+
+    tableSec.style.display = "none";
+    formSec.style.display = "none";
+    nav.style.display = "none";
+    footer.style.display = "none";
+
+
+    // var body=document.getElementsByTagName("body")[0];
+    dialogueBox.style.display = "flex"
+
+
+    // dialogueBox.style.position="absolute"
+
+}
+if(clickbtnthree){clickbtnthree.addEventListener("click", myfunctionthree);}
+function myfunctionthree() {
+
+
+    tableSec.style.display = "none";
+    formSec.style.display = "none";
+    nav.style.display = "none";
+    footer.style.display = "none";
+
+
+    // var body=document.getElementsByTagName("body")[0];
+    dialogueBox.style.display = "flex"
+
+
+    // dialogueBox.style.position="absolute"
+
+}
+if(clickbtnfour){clickbtnfour.addEventListener("click", myfunctionfour);}
+function myfunctionfour() {
 
 
     tableSec.style.display = "none";
@@ -53,9 +107,11 @@ mobNavBtn.addEventListener("click", openmobNav);
 function openmobNav(){ 
     mainbBody.style.display="none";
     mobileNav.style.display="block";
+    tableSec.style.display="none";
 }
 if(mobNavClose){mobNavClose.addEventListener("click", closemobnav);}
 function closemobnav(){
     mainbBody.style.display="block";
     mobileNav.style.display="none";
+    tableSec.style.display="block";
 }
